409a0455875bb093a3b1dfead96e7ec6401effd2
Natalia <natalia.pmd@gmail.com>
2024-07-13 14:55:39 +0200

    Added guitar data

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,52 @@
 import { useState } from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from './data/db'
 
 function App() {
 
-    const [] = useState()
     
-  return (
-    <>
-    <Header/>
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
 
-        <div className="row mt-5">
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-        </div>
-    </main>
+    //API
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     setData(db)
+    // }, [])
+
+    function addToCart(item) {
+        setCart(prevCart => [...prevCart, item])
+
+    }
+  
+    return (
+        <>
+        <Header/>
+        <main className="container-xl mt-5">
+            <h2 className="text-center">Nuestra Colección</h2>
+
+            <div className="row mt-5">
+                {data.map((guitar) => (
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
+                            setCart={setCart}
+                            addToCart={addToCart}
+                        />
+                    )
+                )}
+            </div>
+        </main>
 
 
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
-    </>
-  )
+        <footer className="bg-dark mt-5 py-5">
+            <div className="container-xl">
+                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            </div>
+        </footer>
+        </>
+    )
 }
 
 export default App
